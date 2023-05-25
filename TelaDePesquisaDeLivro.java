36498e1d838fb3c5f3dc87c69154c935e83c82d9
Letyyccia <102097499+Letyyccia@users.noreply.github.com>
2023-05-25 08:24:32 -0300

    Update TelaDePesquisaDeLivro.java

--- TelaDePesquisaDeLivro.java
+++ TelaDePesquisaDeLivro.java
@@ -1,7 +1,42 @@
-//falta fazer esta parte do trabalho!
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.util.List;
+import java.util.Scanner;
 
 public class TelaDePesquisaDeLivro {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o critério de pesquisa:");
+        String criterio = scanner.nextLine();
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("aplicativo");
+        EntityManager em = emf.createEntityManager();
+
+        try {
+            List<Livro> livros = em.createQuery("SELECT l FROM Livro l WHERE l.nome LIKE :nome OR l.edicao LIKE :edicao", Livro.class)
+                    .setParameter("nome", "%" + criterio + "%")
+                    .setParameter("edicao", "%" + criterio + "%")
+                    .getResultList();
+
+            if (!livros.isEmpty()) {
+                System.out.println("Livros encontrados:");
+
+                for (Livro l : livros) {
+                    System.out.println("ID: " + l.getId() + " | Nome: " + l.getNome() + " | Edicao: " + l.getEdicao());
+                }
+            } else {
+                System.out.println("Não foram encontrados livros com o critério de pesquisa fornecido.");
+            }
+
+        } catch (Exception e) {
+            System.out.println("Erro ao tentar pesquisar os livros.");
+        } finally {
+            em.close();
+            emf.close();
+        }
+
 
     }
 }
