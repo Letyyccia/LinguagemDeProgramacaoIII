5b0b242ce1156ae9cd8a7908254c99efc8d5cc93
Letyyccia <102097499+Letyyccia@users.noreply.github.com>
2023-05-25 08:27:19 -0300

    Update TelaDePesquisaDeLivro.java

--- TelaDePesquisaDeLivro.java
+++ TelaDePesquisaDeLivro.java
@@ -15,16 +15,16 @@ public class TelaDePesquisaDeLivro {
         EntityManager em = emf.createEntityManager();
 
         try {
-            List<Livro> livros = em.createQuery("SELECT l FROM Livro l WHERE l.nome LIKE :nome OR l.edicao LIKE :edicao", Livro.class)
+            List<Livro> livros = em.createQuery("SELECT l FROM Livro l WHERE l.nome LIKE :nome OR l.editora LIKE :editora", Livro.class)
                     .setParameter("nome", "%" + criterio + "%")
-                    .setParameter("edicao", "%" + criterio + "%")
+                    .setParameter("editora", "%" + criterio + "%")
                     .getResultList();
 
             if (!livros.isEmpty()) {
                 System.out.println("Livros encontrados:");
 
                 for (Livro l : livros) {
-                    System.out.println("ID: " + l.getId() + " | Nome: " + l.getNome() + " | Edicao: " + l.getEdicao());
+                    System.out.println("ID: " + l.getId() + " | Nome: " + l.getNome() + " | Editora: " + l.getEditora());
                 }
             } else {
                 System.out.println("Não foram encontrados livros com o critério de pesquisa fornecido.");
